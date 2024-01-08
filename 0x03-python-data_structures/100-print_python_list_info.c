2ec640371d5bc2d07b5a3f90b46f328665bb3e66
joewacira <josephwmuthon@gmail.com>
2024-01-08 13:41:47 -0800

    correct

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,11 +1,7 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <Python.h>
-/**
- * print_python_list_info -  func that prints some basic
- * info about Python lists
- * @p: python list
- */
+#include <python.h>
+#include <object.h>
+#include <listobject.h>
+
 void print_python_list_info(PyObject *p)
 {
 	int elem;
