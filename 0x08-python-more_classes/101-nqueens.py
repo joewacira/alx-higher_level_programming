7730c1e8bef1086d313c3447b032d78cc71b5e7c
joewacira <josephwmuthon@gmail.com>
2024-01-29 16:01:41 -0800

    python more classes

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Solves the N-queens puzzle.
 
-Determines all possible solutions to placing N
+Determining all possble solutions to placing N
 N non-attacking queens on an NxN chessboard.
 
 Example:
