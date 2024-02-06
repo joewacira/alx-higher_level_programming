5266e577354cc1e193df33c902644e27d24dd72a
joewacira <josephwmuthon@gmail.com>
2024-02-06 12:06:53 -0800

    python I/O

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Defining a class Student."""
+
+
+class Student:
+    """Representing a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializing new Student.
+
+        Args:
+            first_name (str): First name of the student.
+            last_name (str): Last name of the student.
+            age (int): Age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get dictionary representation of the Student.
+
+        If attrs is list of strngs, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attries of the student.
+
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
